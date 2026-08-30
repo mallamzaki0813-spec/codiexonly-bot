b0cf4455db570f642abac766786b951bac436888
mallamzaki0813-spec <mallamzaki0813@gmail.com>
2026-08-30 14:22:21 +0100

    Update relay.py

--- relay.py
+++ relay.py
@@ -7,16 +7,15 @@ SECRET = os.environ.get("COMPANION_SECRET")
 
 @app.websocket("/companion")
 async def companion(websocket: WebSocket):
+    if websocket.headers.get("X-Companion-Secret") != SECRET:
+        await websocket.close(code=1008)
+        return
+
     await websocket.accept()
 
     try:
-        if websocket.query_params.get("secret") != SECRET:
-            await websocket.close(code=1008)
-            return
-
         while True:
             message = await websocket.receive_text()
             await websocket.send_text(message)
-
     except WebSocketDisconnect:
         pass
